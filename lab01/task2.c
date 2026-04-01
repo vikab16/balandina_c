7f4313caa5cb025ca8cdc1dc500ff1c5e0d9e575
vikab16 <balandinavika127@yandex.ru>
2026-04-01 17:28:57 +0500

    почищены лишние файлы

--- lab01/task2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-    printf("char: %zu\n", sizeof(char));
-    printf("int: %zu\n", sizeof(int));
-    printf("float: %zu\n", sizeof(float));
-    printf("double: %zu\n", sizeof(double));
-    return 0;
-}
--- lab01/task7.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main () {
-    float a = 0.1f;
-    float b = 0.2f;
-    float s = a + b;
-    printf("%.20f\n", s);
-        return 0;
-}
